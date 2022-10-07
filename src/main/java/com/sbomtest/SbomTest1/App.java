a9b2d778623e9fa61d44939df31b44eca8919ed5
Kao, Bai Chien <bai.chien.kao@accenture.com>
2022-10-07 11:43:26 -0400

    first commit

---
+++ src/main/java/com/sbomtest/SbomTest1/App.java
@@ -0,0 +1,20 @@
+package com.sbomtest.SbomTest1;
+
+import com.sun.swing.internal.plaf.basic.resources.basic;
+import com.thoughtworks.xstream.converters.basic.*;
+import com.zaxxer.nuprocess.codec.*;
+import org.apache.batik.*;
+import org.keycloak.common.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
