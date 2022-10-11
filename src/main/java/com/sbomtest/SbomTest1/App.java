6c13ae3723c7ce44378a41fcf37c26b864f8332f
Kao, Bai Chien <bai.chien.kao@accenture.com>
2022-10-11 15:49:08 -0400

    Updated 10112022

--- src/main/java/com/sbomtest/SbomTest1/App.java
+++ src/main/java/com/sbomtest/SbomTest1/App.java
@@ -5,6 +5,7 @@ import com.thoughtworks.xstream.converters.basic.*;
 import com.zaxxer.nuprocess.codec.*;
 import org.apache.batik.*;
 import org.keycloak.common.util.*;
+import org.apache.commons.jxpath.JXPathContext;
 
 /**
  * Hello world!
@@ -14,6 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String newStr = new String("Sbom");
+    	JXPathContext context = JXPathContext.newContext(newStr);
+    	context.equals(newStr);
         System.out.println( "Hello World!" );
         
     }
